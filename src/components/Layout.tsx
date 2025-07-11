857a0fb2aee96e91747d8a8354be5c87c69ab19c
jovan <vuksanovicjovan@hotmail.com>
2025-07-11 13:27:57 +0200

    layout style

---
+++ src/components/Layout.tsx
@@ -0,0 +1,30 @@
+import Sidebar from "./Sidebar";
+
+function Layout() {
+  return (
+    <div className="wrapper">
+      <Sidebar />
+      <div className="main">
+        <div className="header">Header</div>
+        <div className="content-wrapper">
+          <div className="content">
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,48 @@
+import logoutIcon from "../assets/icons/logout-icon.svg";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-logo-wrapper">
+        <div className="sidebar-logo-text">MOVIE REVIEW APP</div>
+      </div>
+      <div className="sidebar-nav-wrapper">
+        <div className="sidebar-item">
+          <span>GLAVNA</span>
+        </div>
+        <div className="sidebar-item">
+          <span>HOME</span>
+        </div>
+        <div className="sidebar-item">
+          <span>MOVIES</span>
+        </div>
+        <div className="sidebar-item">
+          <span>BLOG</span>
+        </div>
+        <div className="sidebar-item">
+          <span>PROFILE</span>
+        </div>
+        <div className="sidebar-item">
+          <span>NOTIFICATIONS</span>
+        </div>
+        <div className="sidebar-item">
+          <span>ADMIN PANEL</span>
+        </div>
+      </div>
+      <div className="sidebar-bottom-wrapper">
+        <div className="profile-info-wrapper">
+          <div className="sidebar-item">Jovan Vuksanovic</div>
+          <div className="sidebar-item">Role</div>
+        </div>
+        <div className="logout">
+          <div>
+            {" "}
+            <img src={logoutIcon} alt="logout icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
